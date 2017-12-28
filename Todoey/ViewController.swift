9c3311bc0841fedd4d68d6d4ac9f0ce902f1a811
Teo Yu Sheng <yusheng.teo@gmail.com>
2017-12-28 09:05:04 +0800

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Yu Sheng Teo on 28/12/17.
+//  Copyright © 2017 Yu Sheng Teo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
